e882220926a54a6db47d01c5544a26aced77ef4b
nayoung511 <nayoungbae511@gmail.com>
2020-08-03 22:18:53 +0900

    Initial Commit

---
+++ C++tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++tetris
+//
+//  Created by 배나영 on 3/8/2020.
+//  Copyright © 2020 Nayoung Bae. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
