639178f9f71bdc281d59f67eb4e9749e83984b35
nayoung511 <nayoungbae511@gmail.com>
2020-08-04 23:31:15 +0900

    pieces header

--- C++tetris/main.cpp
+++ C++tetris/main.cpp
@@ -1,16 +1,11 @@
-//
-//  main.cpp
-//  C++tetris
-//
-//  Created by 배나영 on 3/8/2020.
-//  Copyright © 2020 Nayoung Bae. All rights reserved.
-//
-
 #include <iostream>
+#include <pieces.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
+
+
+
